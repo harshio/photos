b1c4468dc51d5feb8cd0bf146c4fc67ca3ad5fe0
harshio <harshioleti@gmail.com>
2025-04-08 11:10:42 -0400

    added helpful return buttons

--- src/photos/controller/StockController.java
+++ src/photos/controller/StockController.java
@@ -3,18 +3,24 @@ package photos.controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class StockController {
 
     @FXML
     private HBox imageContainer;
     @FXML Button quitButton;
+    @FXML Button returnButton;
 
     @FXML
     public void initialize() {
@@ -31,6 +37,21 @@ public class StockController {
             } else {
                 System.out.println("Image file not found: " + imageFile.getAbsolutePath());
             }
+        }
+    }
+
+    public void loadInStockPage(ActionEvent e){
+        try{
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/photos/view/TrueStockPage.fxml"));
+			Parent root = loader.load();
+			Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+			stage.getScene().setRoot(root);
+
+			stage.setTitle("Album");
+			stage.show();
+		}
+		catch (IOException ex) {
+            ex.printStackTrace(); // Optional: replace with GUI error dialog
         }
     }
 
